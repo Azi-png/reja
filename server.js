6e449f6a1f156b4ba265189f7791ce464e0be36f
Azi-png <azizagoibnazarova24@gmail.com>
2025-02-27 08:38:45 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,53 +1,30 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
+let db;
+const connectionString =
+  "mongodb+srv://goibnazarovaaziza041:OCFf6R2JPfHpftFG@cluster0.mrgbu.mongodb.net/";
 
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on: http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1 kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// 2 Session
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1>HELLO WORLD</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
