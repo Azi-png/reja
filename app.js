7244ff405e2723fff1266e9c7aacbede6d042d27
Azi-png <azizagoibnazarova24@gmail.com>
2025-02-27 15:48:48 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+//MongoDB chaqirish
+const db = require("./server").db();
+
+// 1 kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 2 Session
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+  //code with db
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+module.exports = app;
